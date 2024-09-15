140444343634747b5316cbf747de5bb4d3570b54
pasebukky <olubukunolapase@gmail.com>
2024-09-15 18:54:15 +0100

    Refactor package dependencies and add Redux store for user authentication

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,7 @@
-import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice';
 
 export default function Signin() {
   const [formData, setFormData] = useState({
@@ -7,10 +9,9 @@ export default function Signin() {
     email: '',
     password: '',
   });
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();  
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value });
   };
@@ -18,7 +19,7 @@ export default function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart()); 
       const res = await fetch('/api/auth/signin',
         {
           method: 'POST',
@@ -28,22 +29,18 @@ export default function Signin() {
       );
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
       console.log(data);
     }
     catch (error) {
-      setError(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     } 
   };
 
-  // console.log(formData);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
