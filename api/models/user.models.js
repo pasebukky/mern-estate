7b58830451610fa226ac29da53fe160b5fed71c8
pasebukky <olubukunolapase@gmail.com>
2024-09-16 00:34:54 +0100

    Refactor user avatar default image and add file upload functionality to Profile page

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: 'https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2220431045.jpg',
+        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
 }, { timestamps: true });
 
