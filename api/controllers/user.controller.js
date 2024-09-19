ffde85bd1c6ebdacaa66ef5c6c72ee05e4990ba2
pasebukky <olubukunolapase@gmail.com>
2024-09-19 11:10:32 +0100

    Refactor handleImageSubmit function in CreateListing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import  User from '../models/user.models.js';
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.models.js';
 
 export const test = (req, res) => {
     res.json({ message: "Hello World!" });
@@ -45,6 +46,16 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie('access_token');
     res.status(200).json('User deleted successfully!');
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only view your own listings!'));
+  try {
+    const listings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
